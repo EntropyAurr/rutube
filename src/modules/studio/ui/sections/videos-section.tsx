cc863bddfa0720a6f6fefc4924c00cff66fde548
Aurora <auduong.301@gmail.com>
2026-01-26 22:16:10 +0700

    create video skeleton

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -11,14 +11,73 @@ import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
 import { snakeCaseToTitle } from "@/lib/utils";
 import { format } from "date-fns";
 import { Globe2Icon, LockIcon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function VideosSection() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<VideoSectionSkeleton />}>
       <ErrorBoundary fallback={<p>Error</p>}>
         <VideosSectionSuspense />
       </ErrorBoundary>
     </Suspense>
+  );
+}
+
+function VideoSectionSkeleton() {
+  return (
+    <>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[510px] pl-6">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="pr-6 text-right">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <TableRow key={index}>
+                <TableCell className="pl-6">
+                  <div className="flex items-center gap-4">
+                    <Skeleton className="h-20 w-36" />
+
+                    <div className="flex flex-col gap-2">
+                      <Skeleton className="h-4 w-[100px]" />
+                      <Skeleton className="h-3 w-[150px]" />
+                    </div>
+                  </div>
+                </TableCell>
+
+                <TableCell>
+                  <Skeleton className="h-4 w-20" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-16" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-24" />
+                </TableCell>
+                <TableCell className="pr-1 text-right">
+                  <Skeleton className="ml-auto h-4 w-12" />
+                </TableCell>
+                <TableCell className="pr-6 text-right">
+                  <Skeleton className="ml-auto h-4 w-12" />
+                </TableCell>
+                <TableCell className="pr-4 text-right">
+                  <Skeleton className="ml-auto h-4 w-12" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 }
 
@@ -54,7 +113,7 @@ function VideosSectionSuspense() {
               .map((video) => (
                 <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
                   <TableRow className="cursor-pointer">
-                    <TableCell>
+                    <TableCell className="pl-7">
                       <div className="flex items-center gap-4">
                         <div className="relative aspect-video w-36 shrink-0">
                           <VideoThumbnail title={video.title} duration={video.duration || 0} imageUrl={video.thumbnailUrl} previewUrl={video.previewUrl} />
@@ -66,19 +125,25 @@ function VideosSectionSuspense() {
                         </div>
                       </div>
                     </TableCell>
+
                     <TableCell>
                       <div className="flex items-center">
                         {video.visibility === "private" ? <LockIcon className="mr-2 size-4" /> : <Globe2Icon className="mr-2 size-4" />}
                         {snakeCaseToTitle(video.visibility)}
                       </div>
                     </TableCell>
+
                     <TableCell>
                       <div className="flex items-center">{snakeCaseToTitle(video.muxStatus || "error")}</div>
                     </TableCell>
+
                     <TableCell className="truncate text-sm">{format(new Date(video.createdAt), "d MMM yyyy")}</TableCell>
-                    <TableCell>views</TableCell>
-                    <TableCell>comments</TableCell>
-                    <TableCell>likes</TableCell>
+
+                    <TableCell className="pr-2 text-right text-sm">views</TableCell>
+
+                    <TableCell className="pr-2 text-right text-sm">comments</TableCell>
+
+                    <TableCell className="pr-7 text-right text-sm">likes</TableCell>
                   </TableRow>
                 </Link>
               ))}
