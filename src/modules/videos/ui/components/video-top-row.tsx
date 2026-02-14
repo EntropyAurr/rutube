21ada1951b8ff580ec2a40fa05589afde37e3d79
Aurora <auduong.301@gmail.com>
2026-02-14 22:12:30 +0700

    video page part-1

---
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -0,0 +1,17 @@
+import { VideoGetOneOuput } from "../../types";
+import { VideoOwner } from "./video-owner";
+
+interface VideoTopRowProps {
+  video: VideoGetOneOuput;
+}
+
+export function VideoTopRow({ video }: VideoTopRowProps) {
+  return (
+    <div className="mt-4 flex flex-col gap-4">
+      <h1 className="text-xl font-semibold">{video.title}</h1>
+      <div className="flex flex-col gap-4 sm:flex-row sm:items-start sm:justify-between">
+        <VideoOwner user={video.user} videoId={video.id} />
+      </div>
+    </div>
+  );
+}
---
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { VideoPlayer } from "../components/video-player";
+import { VideoBanner } from "../components/video-banner";
+import { VideoTopRow } from "../components/video-top-row";
+
+interface VideoSectionProps {
+  videoId: string;
+}
+
+export function VideoSection({ videoId }: VideoSectionProps) {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error!</p>}>
+        <VideoSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+}
+
+function VideoSectionSuspense({ videoId }: VideoSectionProps) {
+  const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId });
+
+  return (
+    <>
+      <div className={cn("relative aspect-video overflow-hidden rounded-xl bg-black", video.muxStatus !== "ready" && "rounded-b-none")}>
+        <VideoPlayer onPlay={() => {}} playbackId={video.muxPlaybackId} thumbnailUrl={video.thumbnailUrl} />
+      </div>
+
+      <VideoBanner status={video.muxStatus} />
+
+      <VideoTopRow video={video} />
+    </>
+  );
+}
