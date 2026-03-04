3041bfe5a3b104d3668028eb665be78922a2a922
Aurora <auduong.301@gmail.com>
2026-03-04 21:12:23 +0700

    comment infinite loading - part 1

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import { DEFAULT_LIMIT } from "@/constants";
 import { CommentForm } from "@/modules/comments/ui/components/comment-form";
 import { CommentItem } from "@/modules/comments/ui/components/comment-item";
 import { trpc } from "@/trpc/client";
@@ -21,19 +23,28 @@ export function CommentsSection({ videoId }: CommentsSectionProps) {
 }
 
 function CommentsSectionSuspense({ videoId }: CommentsSectionProps) {
-  const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId });
+  const [comments, query] = trpc.comments.getMany.useSuspenseInfiniteQuery(
+    { videoId, limit: DEFAULT_LIMIT },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  );
 
   return (
     <div className="mt-6">
       <div className="flex flex-col gap-6">
-        <h1>{0} Comments</h1>
+        <h1 className="text-xl font-bold">{0} Comments</h1>
 
         <CommentForm videoId={videoId} />
 
         <div className="mt-2 flex flex-col gap-4">
-          {comments.map((comment) => (
-            <CommentItem key={comment.id} comment={comment} />
-          ))}
+          {comments.pages
+            .flatMap((page) => page.items)
+            .map((comment) => (
+              <CommentItem key={comment.id} comment={comment} />
+            ))}
+
+          <InfiniteScroll isManual hasNextPage={query.hasNextPage} isFetchingNextPage={query.isFetchingNextPage} fetchNextPage={query.fetchNextPage} />
         </div>
       </div>
     </div>
